1ebc4e0fc190d9437ad72f61aa0490d8c23d3f26
Lamia1108 <lamiamecifi@gmail.com>
2023-03-21 18:50:38 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,12 @@
 #include "main.h"
-
 /**
- * main - Entry point
- * Return: Always 0
- */
-
+* main- a program that prints _putchar and returns 0
+*
+* Return: always 0
+*/
 int main(void)
-
 {
-
-	_putchar('-');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
